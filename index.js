12e711eee8ec9b10fe05e3a79a6424f6f1087a38
Stellet <czstellet@gmail.com>
2025-03-31 13:53:50 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -11,30 +11,26 @@ client.once('ready', () => {
 
 client.on('messageCreate', async (message) => {
     if (message.content === '!banka' && !message.author.bot) {
-        // Aktuální čas a datum
         const now = new Date();
-        const datum = now.toLocaleDateString('cs-CZ'); // Formát: DD.MM.YYYY
+        const datum = now.toLocaleDateString('cs-CZ'); 
         const cas = now.toLocaleTimeString('cs-CZ', { 
             hour: '2-digit', 
             minute: '2-digit', 
             second: '2-digit',
-            timeZone: 'Europe/Prague' // Nastavení časové zóny na Prahu
+            timeZone: 'Europe/Prague' 
         });
 
-        // Čas a datum o 1 hodinu později
         const nextHour = new Date(now.getTime() + 60 * 60 * 1000);
         const datum2 = nextHour.toLocaleDateString('cs-CZ');
         const cas2 = nextHour.toLocaleTimeString('cs-CZ', { 
             hour: '2-digit', 
             minute: '2-digit', 
             second: '2-digit',
-            timeZone: 'Europe/Prague' // Nastavení časové zóny na Prahu
+            timeZone: 'Europe/Prague'
         });
 
-        // Pošle zprávu
-        await message.channel.send(`> 💰   ×   Banka byla vykradena **${datum}** ve **${cas}**.\n> ⏳   ×   Další banka půjde **${datum2}** ve **${cas2}**.`);
+        await message.channel.send(`> 💰   ×   Banka byla vykradena **${datum}** ve **${cas}**.\n > \n> ⏳   ×   Další banka půjde **${datum2}** ve **${cas2}**.`);
 
-        // Smazání příkazové zprávy
         try {
             await message.delete();
         } catch (error) {
@@ -44,30 +40,26 @@ client.on('messageCreate', async (message) => {
         }
     }
     if (message.content === '!samoska' && !message.author.bot) {
-        // Aktuální čas a datum
         const now = new Date();
-        const datum = now.toLocaleDateString('cs-CZ'); // Formát: DD.MM.YYYY
+        const datum = now.toLocaleDateString('cs-CZ');
         const cas = now.toLocaleTimeString('cs-CZ', { 
             hour: '2-digit', 
             minute: '2-digit', 
             second: '2-digit',
-            timeZone: 'Europe/Prague' // Nastavení časové zóny na Prahu
+            timeZone: 'Europe/Prague'
         });
 
-        // Čas a datum o 1 hodinu později
         const nextHour = new Date(now.getTime() + 60 * 60 * 1000);
         const datum2 = nextHour.toLocaleDateString('cs-CZ');
         const cas2 = nextHour.toLocaleTimeString('cs-CZ', { 
             hour: '2-digit', 
             minute: '2-digit', 
             second: '2-digit',
-            timeZone: 'Europe/Prague' // Nastavení časové zóny na Prahu
+            timeZone: 'Europe/Prague'
         });
 
-        // Pošle zprávu
-        await message.channel.send(`> 🛒   ×   Sámoška byla vykradena **${datum}** ve **${cas}**.\n> ⏳   ×   Další sámoška půjde **${datum2}** ve **${cas2}**.`);
+        await message.channel.send(`> 🛒   ×   Sámoška byla vykradena **${datum}** ve **${cas}**.\n > \n> ⏳   ×   Další sámoška půjde **${datum2}** ve **${cas2}**.`);
 
-        // Smazání příkazové zprávy
         try {
             await message.delete();
         } catch (error) {
